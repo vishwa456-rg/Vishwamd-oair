ae5668a9afc945e5a2e9fa46bb706bf2384dd685
vishwa456-rg <pkistantayya@gmail.com>
2025-03-10 15:37:24 +0530

    Update config.js

--- config.js
+++ config.js
@@ -9,6 +9,6 @@ function convertToBool(text, fault = 'true') {
 
 
 module.exports = {
-BOT_NAME: process.env.BOT_NAME === undefined ? 'vishwa': process.env.BOT_NAME,
-SEND_MESSAGE: process.env.SEND_MESSAGE === undefined ? 'viwa developer': process.env.SEND_MESSAGE,
+BOT_NAME: process.env.BOT_NAME === undefined ? '': process.env.BOT_NAME,
+SEND_MESSAGE: process.env.SEND_MESSAGE === undefined ? '': process.env.SEND_MESSAGE,
 };
